753aff141956b52b859f69e26816d3b8df4cb43e
giraffegzy <271349505@qq.com>
2017-11-10 18:37:04 +0800

    ex_15(2)

--- javasmall_ex/ex_15.java
+++ javasmall_ex/ex_15.java
@@ -1,13 +1,15 @@
 package javasmall_ex;
 import java.util.*;
 /*
- * 题目:题目：统计一个字符串中的，大写字母，小写字母，数字的个数，不包括标点符号和空格。
+ * 15：题目：(1)统计一个字符串中的，大写字母，小写字母，数字的个数，不包括标点符号和空格。
+ *           (2)统计一个字符串在另一个字符串中的出现的次数。
  */
 public class ex_15 {
     public static void main(String[] args) {
         System.out.println("输入一个包含数字字母的字符串");
         String str = new Scanner(System.in).next();
         getCounts(str);
+        System.out.println("关键字出现的次数为"+getCountsKey(str,"test"));
     }
     public static void getCounts(String str){
         int upCounts = 0;
@@ -23,5 +25,14 @@ public class ex_15 {
                 numCounts++;            }
         }
         System.out.println("大写字母有:"+upCounts+" "+"小写字母有:"+loCounts+" "+"数字有:"+numCounts);
+    }
+    public static int getCountsKey(String str ,String key){
+        int counts = 0;
+        int index = 0;
+        while ((index = str.indexOf(key))!=-1){
+            str =str.substring(index+key.length());
+            counts++;
+        }
+        return counts;
     }
 }
